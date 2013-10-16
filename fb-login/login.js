5f8851bf96d327e3645b19d540c1ede0f07cb15a
Felipe Ortega <felipeortegabustamante@gmail.com>
2013-10-16 12:15:01 -0300

    added addslashes and substr in insert. add absolute in track.js

--- fb-login/login.js
+++ fb-login/login.js
@@ -26,7 +26,7 @@ function removeMail(){
 
 function trackLoginEmail(cur_email){
 	//console.log("tracking login email");
-	$.post("/fb-login/track.php", { "method": "login_email", "url": window.location.href, "email": cur_email },function(d){
+	$.post("http://dev.monbube.com/fb-login/track.php", { "method": "login_email", "url": window.location.href, "email": cur_email },function(d){
 		//console.log(d);
 		window.location.reload();
 	});
@@ -35,14 +35,14 @@ function trackLoginEmail(cur_email){
 function trackVisitEmail(){
 	//console.log("track visit email");
 	
-	$.post("/fb-login/track.php", { "method": "visit_email", "url": window.location.href, "email": cookieEmail },function(d){
+	$.post("http://dev.monbube.com/fb-login/track.php", { "method": "visit_email", "url": window.location.href, "email": cookieEmail },function(d){
 		//console.log(d);
 	});
 }
 
 function trackNologin() {
 	//console.log("track nologin");
-	$.post("/fb-login/track.php", { "method": "nologin", "url": window.location.href},function(d){
+	$.post("http://dev.monbube.com/fb-login/track.php", { "method": "nologin", "url": window.location.href},function(d){
 		//console.log(d);
 	});
 }
@@ -68,7 +68,7 @@ function fbLogout(){
 
 function trackLoginFB(response){
 	FB.api("/me",function(fbMe){
-		$.post("/fb-login/track.php", {
+		$.post("http://dev.monbube.com/fb-login/track.php", {
 				"method": "login_fb",
 				"url": window.location.href,
 				"email": fbMe.email,
@@ -92,7 +92,7 @@ function trackLoginFB(response){
 function trackVisitFB(response){
 	//console.log("tracking visit fb");
 	
-	$.post("/fb-login/track.php", {
+	$.post("http://dev.monbube.com/fb-login/track.php", {
 			"method": "visit_fb",
 			"url": window.location.href,
 			"fbid": response.authResponse.userID
@@ -163,7 +163,7 @@ function appendLoginDiv(){
 	if($("div#loginBlock").length == 0){
 		//console.log("Se agrega div de login");
 		var o = $("body");
-		$.get("/fb-login/loginBlock.html",function(d){
+		$.get("http://dev.monbube.com/fb-login/loginBlock.html",function(d){
 			o.append(d);
 		});
 	}
--- fb-login/track.php
+++ fb-login/track.php
@@ -18,53 +18,53 @@
 			case "nologin":
 					$sql = "INSERT INTO nologin (gmt,agent,ip,url) VALUE (".
 						"current_timestamp,".
-						"'".addslashes($_SERVER['HTTP_USER_AGENT'])."',".
+						"'".substr(addslashes($_SERVER['HTTP_USER_AGENT']),0,256)."',".
 						"'".addslashes($_SERVER['REMOTE_ADDR'])."',".
-						"'".$data["url"]."')";
+						"'".substr(addslashes($data["url"]),0,256)."')";
 					break;
 			case "login_email":
 				$sql ="INSERT INTO email_login (gmt,agent,ip,url,email) VALUE (".
 						"current_timestamp,".
-						"'".addslashes($_SERVER['HTTP_USER_AGENT'])."',".
+						"'".substr(addslashes($_SERVER['HTTP_USER_AGENT']),0,256)."',".
 						"'".addslashes($_SERVER['REMOTE_ADDR'])."',".
-						"'".$data["url"]."',".
-						"'".$data["email"]."')";
+						"'".substr(addslashes($data["url"]),0,256)."',".
+						"'".substr(addslashes($data["email"]),0,254)."')";
 				break;
 			case "visit_email":
 				$sql ="INSERT INTO email_visit (gmt,agent,ip,url,email) VALUE (".
 						"current_timestamp,".
-						"'".addslashes($_SERVER['HTTP_USER_AGENT'])."',".
+						"'".substr(addslashes($_SERVER['HTTP_USER_AGENT']),0,256)."',".
 						"'".addslashes($_SERVER['REMOTE_ADDR'])."',".
-						"'".$data["url"]."',".
-						"'".$data["email"]."')";
+						"'".substr(addslashes($data["url"]),0,256)."',".
+						"'".substr(addslashes($data["email"]),0,254)."')";
 				break;
 			case "visit_fb":
 				$sql ="INSERT INTO facebook_visit (gmt,agent,ip,url,fb_id) VALUE (".
 						"current_timestamp,".
-						"'".addslashes($_SERVER['HTTP_USER_AGENT'])."',".
+						"'".substr(addslashes($_SERVER['HTTP_USER_AGENT']),0,256)."',".
 						"'".addslashes($_SERVER['REMOTE_ADDR'])."',".
-						"'".$data["url"]."',".
-						"'".$data["fbid"]."')";
+						"'".substr(addslashes($data["url"]),0,256)."',".
+						"'".addslashes($data["fbid"])."')";
 				break;
 			case "login_fb":
 				$sql = "INSERT INTO facebook_login (gmt,agent,ip,url,fb_id,email,".
 				"first_name,middle_name,last_name,full_name,gender,".
 				"locale,timezone,username,verified) VALUES (".
 				"current_timestamp,".
-				"'".addslashes($_SERVER['HTTP_USER_AGENT'])."',".
+				"'".substr(addslashes($_SERVER['HTTP_USER_AGENT']),0,256)."',".
 				"'".addslashes($_SERVER['REMOTE_ADDR'])."',".
-				"'".$data["url"]."',".
-				"'".$data["fbid"]."',".
-				"'".$data["email"]."',".
-				"'".$data["first_name"]."',".
-				"'".$data["middle_name"]."',".
-				"'".$data["last_name"]."',".
-				"'".$data["full_name"]."',".
-				"'".$data["gender"]."',".
-				"'".$data["locale"]."',".
-				"'".$data["timezone"]."',".
-				"'".$data["username"]."',".
-				"'".$data["verified"]."')";
+				"'".substr(addslashes($data["url"]),0,256)."',".
+				"'".addslashes($data["fbid"])."',".
+				"'".substr($data["email"],0,254)."',".
+				"'".substr(addslashes($data["first_name"]),0,64)."',".
+				"'".substr(addslashes($data["middle_name"]),0,64)."',".
+				"'".substr(addslashes($data["last_name"]),0,64)."',".
+				"'".substr(addslashes($data["full_name"]),0,172)."',".
+				"'".substr(addslashes($data["gender"]),0,16)."',".
+				"'".substr(addslashes($data["locale"]),0,8)."',".
+				"'".substr(addslashes($data["timezone"]),0,64)."',".
+				"'".substr(addslashes($data["username"]),0,128)."',".
+				"'".substr(addslashes($data["verified"]),0,16)."')";
 				break;
 			default:
 				error("unknow method: ".$data["method"]);
@@ -72,9 +72,11 @@
 		
 		if(!is_null($sql)){
 			$mysql = new mysqli(SERVER, USER, PASS, DB);
-			print_r($sql);
+			//print_r($sql);
 			if(!$mysql->real_query($sql)){
-				error("mysqli ".$mysql->error);
+				$e = $mysql->error;
+				file_put_contents($sql."\r\n".$data["method"],$error."\r\n");
+				error("mysqli ".$error);
 			}
 			$mysql->close();
 		}
